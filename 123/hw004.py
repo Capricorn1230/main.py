18472e59f04b9758d97a5e591f7dc7a392842a1e
Capricorn1230 <BHYJ123456789@163.com>
2018-11-28 16:57:45 +0800

    Signed-off-by: Capricorn1230 <BHYJ123456789@163.com>

---
+++ 123/hw004.py
@@ -0,0 +1,29 @@
+import tornado.ioloop
+import tornado.web
+class MainHandler(tornado.web.RequestHandler):
+    def get(self,n):
+        n = int(n)
+        html='''
+        <html>
+        <body>
+        <table>
+        '''  
+        for i in range(1 ,n+1):
+            html += '<TR>'
+            for j in range ( 1, n+1):
+                 if i <= j:
+                   html += '<TD>%d*%d=%2d</td>'%(i , j , i*j)
+            html += '</TR>'
+        html +='''
+        </table>
+        </body>
+        </html>
+        '''
+        self.write(html)
+application = tornado.web.Application([
+    (r"/", MainHandler),
+], debug = True)
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
